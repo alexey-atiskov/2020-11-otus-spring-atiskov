f7977630f2a51b7b42c0d3b3c969921d0de55069
alexey.atiskov <atiskov@gmail.com>
2020-11-29 17:44:04 +0300

    Second implementation.

---
+++ csv-questions-answers/src/test/java/ru/atiskov/junit/service/QuizServiceImplTest.java
@@ -0,0 +1,15 @@
+package ru.atiskov.junit.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class QuizServiceImplTest {
+}
