fb917482e15208c19d151bad26b60f756cbad1e2
alexey.atiskov <atiskov@gmail.com>
2020-12-26 19:26:17 +0300

    Add more tests.

--- 07-dao-jdbс/src/test/java/ru/atiskov/dao/BookDaoJdbcTest.java
+++ 07-dao-jdbс/src/test/java/ru/atiskov/dao/BookDaoJdbcTest.java
@@ -4,16 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.List;
 
 import ru.atiskov.domain.Book;
-
-import static org.assertj.core.api.Assertions.*;
+// TODO so many classes =(
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @JdbcTest
 @Import(BookDaoJdbc.class)
 class BookDaoJdbcTest {
 
-    public static final int EXPECTED_BOOKS_COUNT = 3;
+    private static final int EXPECTED_BOOKS_COUNT = 3;
+
     @Autowired
     private BookDao bookDao;
 
@@ -31,5 +37,31 @@ class BookDaoJdbcTest {
 
         Book actualBook = bookDao.getById(3);
         assertThat(actualBook).isEqualTo(expectedBook);
+    }
+
+    @Test
+    void getById() {
+        Book expectedBook = new Book(1, 1, 2, "book1");
+
+        Book actualBook = bookDao.getById(1);
+
+        assertThat(actualBook).isEqualTo(expectedBook);
+    }
+
+    @Test
+    void getAll() {
+        List<Book> all = bookDao.getAll();
+        assertEquals("Size of books library", all.size(), EXPECTED_BOOKS_COUNT);
+    }
+
+    @Test
+    void deleteById() {
+        Book expectedBook = new Book(1, 1, 2, "book1");
+        Book actualBook = bookDao.getById(1);
+        assertThat(actualBook).isEqualTo(expectedBook);
+
+        bookDao.deleteById(1);
+
+        assertThrows(EmptyResultDataAccessException.class, () -> bookDao.getById(1));
     }
 }
