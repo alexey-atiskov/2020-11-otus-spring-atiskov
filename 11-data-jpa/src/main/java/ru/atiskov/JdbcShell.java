884c9f1ab1b90eb1f81ab68bc89dba23b8977389
alexey.atiskov <atiskov@gmail.com>
2021-01-31 16:19:46 +0300

    Add more methods to shell scripting.

--- 11-data-jpa/src/main/java/ru/atiskov/JdbcShell.java
+++ 11-data-jpa/src/main/java/ru/atiskov/JdbcShell.java
@@ -12,7 +12,10 @@ import lombok.AllArgsConstructor;
 import ru.atiskov.models.Author;
 import ru.atiskov.models.Book;
 import ru.atiskov.models.Genre;
+import ru.atiskov.repositories.AuthorRepositoryJpa;
 import ru.atiskov.repositories.BookRepositoryJpa;
+import ru.atiskov.repositories.CommentRepositoryJpa;
+import ru.atiskov.repositories.GenreRepositoryJpa;
 
 @ShellComponent
 @AllArgsConstructor
@@ -20,6 +23,12 @@ public class JdbcShell {
 
     @Autowired
     private BookRepositoryJpa bookRepositoryJpa;
+    @Autowired
+    private GenreRepositoryJpa genreRepository;
+    @Autowired
+    private AuthorRepositoryJpa authorRepository;
+    @Autowired
+    private CommentRepositoryJpa commentRepository;
 
     @ShellMethod(key = "countb", value = "Print count of all books")
     public void start() {
@@ -27,11 +36,17 @@ public class JdbcShell {
     }
 
     @ShellMethod(key = "addb", value = "Add book")
+    @Transactional
     public void addBookToLibrary(
             @ShellOption({"bookname", "bn"}) String bookName,
-            @ShellOption({"author", "a"}) String author,
-            @ShellOption({"genre", "g"}) String genre) {
-        bookRepositoryJpa.save(new Book(List.of(new Author(author)), new Genre(genre), bookName));
+            @ShellOption({"author", "a"}) String authorStr,
+            @ShellOption({"genre", "g"}) String genreStr) {
+        Author author = new Author(authorStr);
+        authorRepository.save(author);
+        Genre genre = new Genre(genreStr);
+        genreRepository.save(genre);
+        Book book = new Book(List.of(author), genre, bookName);
+        bookRepositoryJpa.save(book);
     }
 
     @ShellMethod(key = "getbi", value = "Get book infos")
@@ -45,6 +60,20 @@ public class JdbcShell {
     public void getBookInfoById(
             @ShellOption({"book id", "bi"}) long bookId) {
         System.out.println("Book info is = " + bookRepositoryJpa.findById(bookId));
+    }
+
+    @ShellMethod(key = "fb", value = "Get book info by name")
+    @Transactional(readOnly = true)
+    public void getBookInfoByName(
+            @ShellOption({"book name", "bn"}) String bookName) {
+        System.out.println("Book info is = " + bookRepositoryJpa.findByName(bookName));
+    }
+
+    @ShellMethod(key = "fba", value = "Get book info by author's name")
+    @Transactional(readOnly = true)
+    public void getBookInfoByAuthorName(
+            @ShellOption({"author name", "an"}) String authorName) {
+        System.out.println("Book info is = " + bookRepositoryJpa.findByAuthors_firstName(authorName));
     }
 
     @ShellMethod(key = "del", value = "Delete all info")
